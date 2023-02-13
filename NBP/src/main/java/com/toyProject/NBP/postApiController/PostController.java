909d927e06e4d987798b7e93482bdac323b36a64
220SRS <jesikrjy@gmail.com>
2023-02-13 21:07:31 +0900

    2022/02/13 dto 작성

--- NBP/src/main/java/com/toyProject/NBP/postApiController/PostController.java
+++ NBP/src/main/java/com/toyProject/NBP/postApiController/PostController.java
@@ -1,4 +1,11 @@
 package com.toyProject.NBP.postApiController;
 
+import com.toyProject.NBP.postRepository.PostRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class PostController {
+    private final PostService postService;
+    private final PostRepository postRepository;
+
+    @GetMapping
 }
--- NBP/src/main/java/com/toyProject/NBP/postRepository/PostRepository.java
+++ NBP/src/main/java/com/toyProject/NBP/postRepository/PostRepository.java
@@ -1,4 +1,5 @@
 package com.toyProject.NBP.postRepository;
 
 public class PostRepository {
+
 }
