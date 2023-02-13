9bda0699f215c196f9793c120ec9d86735510442
220SRS <jesikrjy@gmail.com>
2023-02-13 21:43:41 +0900

    branch test1

--- NBP/src/main/java/com/toyProject/NBP/postApiController/PostController.java
+++ NBP/src/main/java/com/toyProject/NBP/postApiController/PostController.java
@@ -4,8 +4,4 @@ import com.toyProject.NBP.postRepository.PostRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class PostController {
-    private final PostService postService;
-    private final PostRepository postRepository;
-
-    @GetMapping
 }
